2a008b1e6002adb560e33dfbe47437b2a19224cd
deb <debkbanerji@gmail.com>
2016-07-08 05:07:19 -0400

    Scraping multiple monuments
    Code now attempts to scrape data for multiple monuments

--- monumentsofdelhi.py
+++ monumentsofdelhi.py
@@ -1,3 +1,4 @@
+import json
 import urllib
 from bs4 import BeautifulSoup
 
@@ -9,7 +10,7 @@ soup = BeautifulSoup(html_source, 'html.parser')
 
 monument_panels = soup.findAll("div", {"class": "panel-body"})
 
-print monument_panels.__len__()
+print "Scraping data for " + str(monument_panels.__len__()) + " monuments...\n"
 
 monument_links = []
 for panel in monument_panels:
@@ -20,41 +21,49 @@ for panel in monument_panels:
 
 monuments = []
 
-# for link in monument_links:
+for link in monument_links:
 
-link = monument_links[0]
-uopen = urllib.urlopen(link)
-html_source = uopen.read()
+    uopen = urllib.urlopen(link)
+    html_source = uopen.read()
 
-soup = BeautifulSoup(html_source, 'html.parser')
-monument_name = soup.h1.contents[1].string
+    soup = BeautifulSoup(html_source, 'html.parser')
+    monument_name = soup.h1.contents[1].string
 
-panel = soup.findAll("div", {"class": "panel-warning"})
-# print panel[0]
-keys = panel[0].findAll("th", {"class": "col-xs-4"})
-values = panel[0].findAll("td", {"class": "col-xs-8"})
-# print keys
-# print len(keys)
-# print values
-# print len(values)
-monument_dict = {}
-for i in range(0, len(keys)):
-    print ("\"" + str((keys[i].contents[0] or [""])) + "\": \"" + str(values[i].contents[0]) + "\",")
-    if len(keys[i].contents[0].contents) > 0:
-        # print keys[i].contents[0].contents[0]
-        monument_dict[str(keys[i].contents[0].contents[0])] = str(values[i].contents[0])
-    else:
-        monument_dict[str(keys[i - 1].contents[0].contents[0])] = monument_dict[str(
-            keys[i - 1].contents[0].contents[0])] + ", " + str(values[i].contents[0])
+    panel = soup.findAll("div", {"class": "panel-warning"})
+    # print panel[0]
+    keys = panel[0].findAll("th", {"class": "col-xs-4"})
+    values = panel[0].findAll("td", {"class": "col-xs-8"})
+    # print keys
+    # print len(keys)
+    # print values
+    # print len(values)
+    monument_dict = {}
+    # print "\n"
+    for i in range(0, len(keys)):
+        if len(values[i].contents) > 0:
+            value = str(values[i].contents[0])
+        else:
+            value = ""
+        # print ("\"" + str((keys[i].contents[0] or [""])) + "\": \""
+        #        + value + "\",")
+        if len(keys[i].contents[0].contents) > 0:
+            # print keys[i].contents[0].contents[0]
+            monument_dict[str(keys[i].contents[0].contents[0])] = value
+        else:
+            monument_dict[str(keys[i - 1].contents[0].contents[0])] = monument_dict[str(
+                keys[i - 1].contents[0].contents[0])] + "\n" + value
 
-if monument_dict['Coordinates']:
-    coordinates = monument_dict['Coordinates'].split(', ')
-    monument_dict['Latitude'] = coordinates[0]
-    monument_dict['Longitude'] = coordinates[1]
-    del monument_dict['Coordinates']
+    if 'Coordinates' in monument_dict:
+        coordinates = monument_dict['Coordinates'].split(', ')
+        monument_dict['Latitude'] = coordinates[0]
+        monument_dict['Longitude'] = coordinates[1]
+        del monument_dict['Coordinates']
 
+    monuments.append(monument_dict)
+    print monument_dict['Name']
 
-print "\n"
-print monument_name
-for k, v in monument_dict.items():
-    print(k, v)
+with open('monumentsofdelhi.json', 'w') as fp:
+    json.dump(monuments, fp)
+
+print "\nFinished"
+print "Check file \'monumentsofdelhi.json\' for data"
