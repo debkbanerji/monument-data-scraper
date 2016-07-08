5566e52c1eb9dd90aa297c2b179b7fd415417bc1
deb <debkbanerji@gmail.com>
2016-07-08 08:18:59 -0400

    'Description' data
    Now scraping 'Description' data

--- monumentsofdelhi.py
+++ monumentsofdelhi.py
@@ -18,14 +18,11 @@ soup = BeautifulSoup(html_source, 'html.parser')
 
 monument_panels = soup.findAll("div", {"class": "panel-body"})
 
-print "Scraping data for " + str(monument_panels.__len__()) + " monuments...\n"
+print("Scraping data for " + str(monument_panels.__len__()) + " monuments...\n")
 
 monument_links = []
 for panel in monument_panels:
     monument_links.append("http://www.monumentsofdelhi.com/" + str(panel.contents[9].contents[1].get('href')))
-    # for child in panel.contents:
-    #     # link = panel.descendants
-    #     # print link
 
 output_array = []
 output_map = {}
@@ -39,24 +36,16 @@ for link in monument_links:
     # monument_name = soup.h1.contents[1].string
 
     panel = soup.findAll("div", {"class": "panel-warning"})
-    # print panel[0]
     keys = panel[0].findAll("th", {"class": "col-xs-4"})
     values = panel[0].findAll("td", {"class": "col-xs-8"})
-    # print keys
-    # print len(keys)
-    # print values
-    # print len(values)
+
     monument_dict = {}
-    # print "\n"
     for i in range(0, len(keys)):
         if len(values[i].contents) > 0:
             value = str(values[i].contents[0])
         else:
             value = ""
-        # print ("\"" + str((keys[i].contents[0] or [""])) + "\": \""
-        #        + value + "\",")
         if len(keys[i].contents[0].contents) > 0:
-            # print keys[i].contents[0].contents[0]
             monument_dict[str(keys[i].contents[0].contents[0])] = value
         else:
             monument_dict[str(keys[i - 1].contents[0].contents[0])] = monument_dict[str(
@@ -69,13 +58,18 @@ for link in monument_links:
         del monument_dict['Coordinates']
 
     monument_name = monument_dict['Name']
+    excerpt_span = soup.findAll("span", {"id": "cpContent_lblExcerpt"})
+
+    if len(excerpt_span[0].contents) > 0:
+        description = excerpt_span[0].contents[0]
+        monument_dict['Description'] = description
 
     # Removing name from monument_dict and saving to output_map
     monument_dict_for_map = removekey(monument_dict, 'Name')
     output_map[monument_name] = monument_dict_for_map
 
     output_array.append(monument_dict)
-    print monument_dict['Name']
+    print(monument_dict['Name'])
 
 path = os.path.join(os.getcwd(), "output")
 
@@ -94,5 +88,5 @@ with open(os.path.join(path, 'monumentsofdelhi.array.pretty.json'), 'w') as fp:
 with open(os.path.join(path, 'monumentsofdelhi.map.pretty.json'), 'w') as fp:
     json.dump(output_map, fp, False, True, True, True, None, 2, None, 'utf-8', None, True)
 
-print "\nFinished"
-print "Check \'output\' folder for data"
+print("\nFinished")
+print("Check \'output\' folder for data")
