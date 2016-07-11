9066485c3031efb25124e2926a4f52ac8fb51582
deb <debkbanerji@gmail.com>
2016-07-11 14:13:40 +0530

    Image Extraction
    Successfully extracting low resolution images

--- Google Culture/google_culture.py
+++ Google Culture/google_culture.py
@@ -1,7 +1,55 @@
+import codecs
 import json
 import os
+import re
+import requests
 import urllib
 from bs4 import BeautifulSoup
+
+
+def extract_raw_image(target_url, directory_name, file_name=None):
+    output_path = os.path.join(os.getcwd(), "output", directory_name)
+
+    if not file_name:
+        url_split = target_url.split("/")
+        file_name = url_split[len(url_split) - 1]
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    f = open(os.path.join(output_path, file_name), "ab")
+    f.write(requests.get(target_url).content)
+    f.close()
+
+
+def extract_image(page_url, directory_name):
+    uopen = urllib.urlopen(page_url)
+    html_source = uopen.read()
+    html_source = html_source.decode('ascii', 'ignore')
+
+    soup = BeautifulSoup(html_source, 'html.parser')
+    heading = soup.findAll("h1", {"class": "EReoAc"})
+
+    image_name = heading[0].string
+
+    search_obj = re.search(r'"http(.*)ggpht.com(.*)"', html_source)
+    components = str(search_obj.group()).split("\"")
+    image_url = components[1]
+    extract_raw_image(image_url, directory_name, image_name)
+
+
+def recursive_div_scan(div):
+    if hasattr(div, "name") and div.name == "a":
+        image_links.append(div.get("href"))
+        return div.get("href")
+    if hasattr(div, "contents"):
+        result = []
+        for element in div.contents:
+            result.append(recursive_div_scan(element))
+    else:
+        result = str(div)
+    return result
+
 
 path = os.path.join(os.getcwd(), "output")
 
@@ -14,14 +62,32 @@ exhibit_links = ["/culturalinstitute/beta/exhibit/QRUxWeZM", "/culturalinstitute
                  "/culturalinstitute/beta/exhibit/QRWvANZ3", "/culturalinstitute/beta/exhibit/QRU8El0t"]
 # exhibit_links = ["/culturalinstitute/beta/exhibit/QRUxWeZM"]
 
+
 # for each page
 for link in exhibit_links:
-    url = "https://www.google.com/culturalinstitute/beta/u/0/search/exhibit?p=indian-national-trust-for-art-and-cultural-heritage"
     uopen = urllib.urlopen("https://www.google.com" + link)
     html_source = uopen.read()
+    html_source = html_source.decode('ascii', 'ignore')
     soup = BeautifulSoup(html_source, 'html.parser')
     divs = soup.findAll("div", {"class": "PECUof"})
-    print divs
+
+    image_links = []
+    div_array = []
+    for div in divs:
+        div_array.append(recursive_div_scan(div))
+
+    for link in image_links:
+        # Extract image from page
+        link = "https://www.google.com" + link
+        folder_name = link.split("/")[6]
+        print "Extracting image from" + link + " to " + folder_name
+        try:
+            extract_image(link, folder_name)
+        except Exception:
+            print "Error extracting: page may not be in expected format"
+
+    with open(os.path.join(path, 'results.json'), 'w') as fp:
+        json.dump(div_array, fp, False, True, True, True, None, 2, None, 'utf-8', None, True)
 
 print("\nFinished")
 print("Check \'output\' folder for data")
